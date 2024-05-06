c1fce756eb76461e8da1a43741439975e470e842
chesneynl <c.buitendijk@gracious.nl>
2024-05-06 21:04:42 +0200

    add files

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,50 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { FAQ, LatestWork, ShortText, Slider } from '~/components';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: 'Maatwerk Timmerwerk en Renovaties | Stulen bouwservice | Omgeving Utrecht ' },
+        {
+            name: 'description',
+            content:
+                'Ontdek topkwaliteit in timmerwerk, burgerlijke en utiliteitsbouw, en renovatie werkzaamheden door uw lokale expert. Betrouwbaar, duurzaam en op maat gemaakt voor al uw bouwbehoeften. Neem contact op voor uw project!',
+        },
+    ];
 };
 
+// export const loader = async ({ params }) => {
+//     console.log({ params });
+//     const contact = null;
+
+//     if (!contact) {
+//         throw new Response('Not Found', { status: 404 });
+//     }
+//     return json({ contact });
+// };
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    // const { contact } = useLoaderData<typeof loader>();
+
+    // console.log({ contact });
+
+    return (
+        <div className="w-full">
+            <Slider />
+            <div className="container px-4 pb-8 pt-16 sm:px-6 lg:px-8 lg:pt-24 text-center">
+                <h2 className="text-xl font-extrabold text-gray-900 sm:text-3xl">
+                    Ontdek Vakmanschap in Bouw en Renovatie bij Stulen bouwservice
+                </h2>
+
+                <p className="mx-auto mt-4 text-gray-500">
+                    Welkom bij Stulen bouwservice, uw betrouwbare partner voor alle timmerwerken, burgerlijke en
+                    utiliteitsbouw en renovatieprojecten. Met jarenlange ervaring in de bouwsector bieden wij u
+                    vakmanschap en precisie in elk project, groot of klein. Wij staan garant voor maatwerkoplossingen
+                    die perfect aansluiten bij uw wensen en behoeften. Onze toewijding aan kwaliteit en duurzaamheid
+                    maakt ons dé keuze voor uw volgende bouwproject in omgeving Utrecht. Ontdek wat wij voor u kunnen
+                    betekenen en neem vandaag nog contact met ons op voor een vrijblijvende offerte.
+                </p>
+            </div>
+            <LatestWork />
+        </div>
+    );
 }
